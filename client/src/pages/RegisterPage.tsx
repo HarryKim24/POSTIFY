88303f55b04ba3e1cd0b9baac873ddebda4452a8
harry <tl9434@naver.com>
2025-01-19 20:00:40 +0900

    ✨ feat: Redirect to login page after successful registration

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import API from '../utils/api';
 
 const RegisterPage = () => {
@@ -9,38 +10,23 @@ const RegisterPage = () => {
     password: '',
   });
   const [message, setMessage] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const validateInput = () => {
-    if (formData.username.length < 3) {
-      setMessage('사용자 이름은 최소 3자 이상이어야 합니다.');
-      return false;
-    }
-    if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(formData.email)) {
-      setMessage('유효한 이메일을 입력해주세요.');
-      return false;
-    }
-    if (formData.password.length < 8) {
-      setMessage('비밀번호는 최소 8자 이상이어야 합니다.');
-      return false;
-    }
-    return true;
-  };
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!validateInput()) {
-      return;
-    }
     try {
       const response = await API.post('/auth/register', formData);
       setMessage('회원가입이 성공적으로 완료되었습니다.');
       console.log(response.data);
+
       setFormData({ username: '', email: '', password: '' });
+
+      setTimeout(() => navigate('/login'), 1000);
     } catch (error: any) {
       setMessage(error.response?.data?.error || '회원가입에 실패했습니다.');
       console.error(error.response?.data);
