3aa86ed35247a83103dbadf820c6e06bcc8a0b53
harry <tl9434@naver.com>
2025-01-23 15:51:19 +0900

    url

--- server/server.ts
+++ server/server.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: 'https://postify-livid.vercel.app/',
     credentials: true,
   })
 );
